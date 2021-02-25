8c9ab7fd7826a04d61591bdeca409c1a57e93ba8
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-26 01:08:19 +0530

    Use forEach to demonstrate iteration

--- MathOperationApp.java
+++ MathOperationApp.java
@@ -1,28 +1,56 @@
 package com.blbz;
 
-@FunctionalInterface
-interface IMathFunction{
-	int calculate(int a,int b);
-	static void printResult(int a,int b,String function,IMathFunction fobj) {
-		System.out.println("Result is " + function + " is " + fobj.calculate(a, b));}
-}
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.Iterator;
+
 public class MathOperationApp {
 
 	public static void main(String[] args) {
-		IMathFunction add = Integer::sum;
-		
-		IMathFunction sub = (x,y)->x-y;
-		IMathFunction multiply = (x,y)->x*y;
-		IMathFunction divide = (x,y)->x/y;
-	
-		System.out.println("Addition is " + add.calculate(5, 3));
-		System.out.println("Subtraction is " + sub.calculate(5, 3));
-		System.out.println("Multiplication is " + multiply.calculate(5, 3));
-		System.out.println("Divide is " + divide.calculate(5, 3));
-		
-		IMathFunction.printResult(6, 3, "Addition", add);
-		IMathFunction.printResult(6, 3, "Subtraction", sub);
-		IMathFunction.printResult(6, 3, "Divide", divide);
-		IMathFunction.printResult(6, 3, "Multiplication", multiply);
+
+		List<Integer> myList = new ArrayList<Integer>();
+		for(int i =0;i<5;i++) myList.add(i);
+
+		Iterator<Integer> it = myList.iterator();
+		while(it.hasNext()) {
+			Integer i = it.next();
+			System.out.println("Mth1: Iterator Value ::"+i);
+
+		}
+
+		class MyConsumer implements Consumer<Integer>{
+			public void accept(Integer t) {
+				System.out.println("Mth2: Consumer impl Value ::"+t);}
+		}
+		MyConsumer action = new MyConsumer();
+		myList.forEach(action);
+
+		myList.forEach(new Consumer<Integer t>() {
+			public void accept(Integer t) {
+				System.out.println("Mth3:for Each anonyms class values::"+t);
+			}
+		});
+
+		Consumer<Integer>myListAction = n->{
+			System.out.println("Mth4: forEach Lamda imp Value::"+n);
+		};
+		myList.forEach(myListAction);
+
+		myList.forEach(n->{
+			System.out.println("Mth4: forEach Lamda imp Values::"+n);
+		});
 	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
