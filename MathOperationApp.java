a8630b2139947795348663d162b04a0aec818243
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-26 20:08:51 +0530

    Ability to transform each element to double,use stream.map function

--- MathOperationApp.java
+++ MathOperationApp.java
@@ -2,6 +2,9 @@ package com.blbz;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class MathOperationApp {
 
@@ -9,10 +12,24 @@ public class MathOperationApp {
 
 		List<Integer> myList = new ArrayList<Integer>();
 		for(int i =0;i<5;i++) myList.add(i);
-
+		
+		
+		Function<Integer,Double>toDoubleFunction = Integer::doubleValue;
+		Predicate<Integer> isEvenFunction = n -> n%2 == 0;
+		
+		//Processing the stream
 		myList.stream().forEach(n-> {
 			System.out.println("Mth8: Stream forEach Values: "+ n);
 		});
+		
+		//Processing the stream,Apply Operation on then
+		//Store the result
+		List<Double> streamList = myList.stream()
+								  .filter(isEvenFunction)
+								  .map(toDoubleFunction)
+								  .collect(Collectors.toList());
+		System.out.println("Mth9: Printing Double List: " +streamList);
+								   	
 	}
 }
 
